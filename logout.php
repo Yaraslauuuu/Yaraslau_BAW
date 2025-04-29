519eeeaa60f499ec174878c5bee1db2f436a6ccd
Yaraslauuuu <124705302+Yaraslauuuu@users.noreply.github.com>
2025-04-29 02:09:08 +0200

    Delete logout.php

--- logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once dirname(__FILE__, 3).'/config.php';
-
-session_start();
-$_SESSION = array(); // Очищаем массив сессии
-session_destroy();
-header("Location: "._APP_URL."/app/security/login.php");
-exit();
-?>
